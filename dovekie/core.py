0f8913f9bf2cca5c6cfba773a38adab06d9fd797
Conor Hoekstra <codereport@outlook.com>
2024-04-03 17:44:49 -0400

    :bug::bookmark: fix + bump version

--- dovekie/core.py
+++ dovekie/core.py
@@ -1,5 +1,5 @@
 # combinators
-_b     = lambda f, g:    lambda x: f(g(x))
+_b     = lambda f, g:    lambda x:    f(g(x))
 _phi1_ = lambda f, g, h: lambda x, y: g(f(x, y), h(x, y))
 _l_    = lambda x, _: x
 _r_    = lambda _, y: y
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dovekie",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     description="A library that defines common SKI combinators from Combinatory Logic.",
     long_description=open("README.md").read(),
